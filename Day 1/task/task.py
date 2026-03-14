af15e631b990a1841633a074efe21390cd20b339
Great-Osuya <osuyagoc@gmail.com>
2026-03-14 23:14:35 +0100

    ---learn variable labeling/storage---

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,36 @@
-# input() is a function that gives the user a prompt that they fill or answer
-print("Hello " + input("What is your name?"))
-print("Hello " + input("What is your name?") + "!")
-# ('#') Hashtag renders a line not code
-# 'ctrl + /' does exact same thing as hashtag
+# you can name your variable anything just make ure it is readable and as specific as possible.
+# avoid using functions as the names of your variable "print", "input", etc.
+name = input("What is your name")
+print(name)
+
+x = "John"
+print(x)
+
+x = "Mark"
+print(x)
+# note that the functon "len()," is to determine number of characters.
+print(len(name))
+
+# Pause 1
+print(len(input("What is your name?")))
+# Pause 2
+username= input("Username")
+length= len(username)
+print(length)
+
+# below is how you switch variables
+name = input("What is your name")
+gender = input("gender")
+name, gender = gender, name
+print(name, gender)
+print(gender, name)
+# or you could switch like this. I am keeping the above name and gender variables the same below
+name = input("What is your name")
+gender = input("gender")
+temp= name
+name= gender
+gender= temp
+print(name, gender)
+print(gender, name)
+print(temp)
+# you can use anything as the temporary variable. Most people use "temp" but any order random thing is fine
