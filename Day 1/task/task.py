104b293023a7d0238bff69e3016be36c09b7b14b
Great-Osuya <osuyagoc@gmail.com>
2026-03-14 23:05:17 +0100

    ---learn print function---

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+# ("") exclamation marks are required to show that something is a string of characters which the function would act on
+print("Hello World")
+print("Hello Me")
+# ('#') Hashtag renders a line not code
+# 'ctrl + /' does exact same thing as hashtag
