34cb63fb201a50fda0c3135ac4f7d6cbb8cd9c67
Great-Osuya <osuyagoc@gmail.com>
2026-03-14 23:08:53 +0100

    ---learn string manipulation---

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,6 +1,19 @@
 # Write your code below this line 👇
-# ("") exclamation marks are required to show that something is a string of characters which the function would act on
-print("Hello World")
-print("Hello Me")
-# ('#') Hashtag renders a line not code
-# 'ctrl + /' does exact same thing as hashtag
+# \n to make multiple lines with only one line of code. Leave no space between the last variable and "\n"
+# + sign is squeeze multiple strings into one line
+print("Hello" + " " + "Great")
+
+print("Hello Great\nHello Rose\nHello Matthew")
+print("1. Hello World!\n2. Hello World!\n3. Hello World!\n4. Hello World!")
+
+print("Hello Ada!\n" + "0 " + "Hello Ada!\n" + "Hello Ada!")
+
+# the three below have the exact same result
+print("Hello " + "Greg")
+print("Hello" + " Greg")
+print("Hello" + " " + "Greg")
+
+# observer that '\n + space + \n' gives an extra line between the strings. Hence, empty space is a string on its own
+print("Hello Wendy!\n \nHello Wendy!")
+
+print("Hello Angela." + " I am excited to meet you")
