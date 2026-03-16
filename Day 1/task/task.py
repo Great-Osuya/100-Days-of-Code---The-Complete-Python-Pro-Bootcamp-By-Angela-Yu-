b5e7d2e9d6071851f5d8b1c5c409dcd53e108b17
Great-Osuya <osuyagoc@gmail.com>
2026-03-16 22:07:49 +0100

    ---learn input function---

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,14 +1,5 @@
-# This is how I solved the project.
-print("Welcome to GREAT's Band Name Generator.")
-print("What is the name of the city you grew up in?")
-city_name= input()
-print("What is the name of your pet")
-pet_name = input()
-print("Your brand name could be " + city_name + " " + pet_name)
-
-print("\n")
-#The below is how ANGELA YU solved it
-print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's the name of your pet?\n")
-print("Your band name could be " + city + " " + pet)
+# input() is a function that gives the user a prompt that they fill or answer
+print("Hello " + input("What is your name?"))
+print("Hello " + input("What is your name?") + "!")
+# ('#') Hashtag renders a line not code
+# 'ctrl + /' does exact same thing as hashtag
