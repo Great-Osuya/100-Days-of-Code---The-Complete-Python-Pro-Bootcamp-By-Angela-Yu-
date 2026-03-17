c6ad038477f874bcac99ff7d48a4f5c5a1f6427d
Great-Osuya <osuyagoc@gmail.com>
2026-03-17 09:44:10 +0100

    ---learn 'If Else' statement

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,20 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you cannot ride the rollercoaster!")
+
+#Comparator Operators
+# '>' Greater than
+# '<' Less than
+# '>=' Greater than or equal to
+# '<= Less than or equal to
+# '==' Equal to
+# '!=' Not equal to
+
+# The main difference between '=' and '==' is this:
+# '=' means you're assigning the value on the right side to the variable on the left side.
+# While '==' serves as a comparison tool to determine if two values are the same.
+# '=' is used for variable assignment, while '==' is used to check equality.
