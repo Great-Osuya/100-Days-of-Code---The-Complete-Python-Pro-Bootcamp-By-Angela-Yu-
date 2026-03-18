8816843c5c371cbabe75f7c7e39c501bba9dd8d8
Great-Osuya <osuyagoc@gmail.com>
2026-03-18 12:23:55 +0100

    [NEW]---Treasure Island Project---

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -1,3 +1,4 @@
+#The treasure box was obtained from 'ascii.co.uk
 print(r'''
 *******************************************************************************
           |                   |                  |                     |
@@ -22,3 +23,58 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+print("You're at a cross road. Where do you want to go?")
+first_choice = input("Type \"left\" or \"right\".\n ").lower()
+
+
+if first_choice == "left":
+    print("You've come to a lake. There is an island in the middle of the lake.")
+
+    second_choice = input('Type "wait" to wait for a boat. Type "swim" to swim across.\n ').lower()
+    if second_choice == "wait":
+        print("You arrive at the island unharmed. There is a house with 3 doors.")
+
+        third_choice = input("One red, one yellow and one blue. Which colour do you choose?\n ").lower()
+        if third_choice == "yellow":
+            print("You found the treasure! You Win!")
+        elif third_choice == "blue":
+            print("You enter a room of beasts. Game Over!")
+        elif third_choice == "red":
+            print("It's a room full of fire. Game Over!")
+        else:
+            print("Wrong input. Please try again.")
+
+    elif second_choice == "swim":
+        print("You get attacked by an angry trout. Game Over.")
+    else:
+        print("Wrong input. Please try again.")
+
+elif first_choice == "right":
+    print("You fell into a hole. Game Over.")
+else:
+    print("Wrong input. Please try again.")
+
+
+
+#In Python, the backslash (\) is an escape character.
+#It tells Python: "The very next character is part of the text, not part of the code."
+#Python uses double quotes (" ") to mark the beginning and end of a string. If you wrote:
+#input("Type "left" or "right". ")
+#Python would think the string ends at the second quote (right before the word left). It would then get confused........
+#by the rest of the line and throw a SyntaxError.
+#By using \", you are saying: "Don't end the string here; just print a literal double-quote character on the screen."
+
+
+
+#You can avoid the backslashes entirely by mixing single and double quotes:
+#Use single quotes for the outside:
+#input('Type "left" or "right". '), like I did above in 'second_choice '.
+#Use double quotes for the outside (and singles inside):
+#input("Type 'left' or 'right'. ")
+
+
+
+#In an 'input()' function, '.lower()' is used to standardize the user's answer.
+#It converts whatever the user types into all lowercase letters, which prevents your if statements from breaking just...
+#because someone used a Capital Letter.
+
