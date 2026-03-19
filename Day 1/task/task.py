5fa5955c14775e074651e277abbac59c8044483e
Great-Osuya <osuyagoc@gmail.com>
2026-03-19 19:07:29 +0100

    ---learn variable naming---

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,6 +1,10 @@
-# Write your code below this line 👇
-# ("") exclamation marks are required to show that something is a string of characters which the function would act on
-print("Hello World")
-print("Hello Me")
-# ('#') Hashtag renders a line not code.
-# 'ctrl + /' does exact same thing as hashtag
+name = "Angela"
+length = len(name)
+print(length)
+# RULES (and yes, I know the rules are already there).
+# 1. Make sure your variable names are descriptive
+# 2. Don't have spaces between words. Use underscores if space is absolutely necessary
+# 3. Don't start with numbers
+# 4. Don't use special words like print or input
+# 5. Choose simple words that are less likely to become typos
+# 6. Check the company style guidelines if you start work at a company
