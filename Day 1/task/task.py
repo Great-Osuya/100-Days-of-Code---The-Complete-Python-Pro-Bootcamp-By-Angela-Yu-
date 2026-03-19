0617bc53b2d0db451a7a17ee4f3ced2264041cd8
Great-Osuya <osuyagoc@gmail.com>
2026-03-19 19:06:53 +0100

    ---learn printing---

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,36 +1,6 @@
-# you can name your variable anything just make ure it is readable and as specific as possible.
-# avoid using functions as the names of your variable "print", "input", etc.
-name = input("What is your name")
-print(name)
-
-x = "John"
-print(x)
-
-x = "Mark"
-print(x)
-# note that the functon "len()," is to determine number of characters.
-print(len(name))
-
-# Pause 1
-print(len(input("What is your name?")))
-# Pause 2
-username= input("Username")
-length= len(username)
-print(length)
-
-# below is how you switch variables
-name = input("What is your name")
-gender = input("gender")
-name, gender = gender, name
-print(name, gender)
-print(gender, name)
-# or you could switch like this. I am keeping the above name and gender variables the same below
-name = input("What is your name")
-gender = input("gender")
-temp= name
-name= gender
-gender= temp
-print(name, gender)
-print(gender, name)
-print(temp)
-# you can use anything as the temporary variable. Most people use "temp" but any order random thing is fine
+# Write your code below this line 👇
+# ("") exclamation marks are required to show that something is a string of characters which the function would act on
+print("Hello World")
+print("Hello Me")
+# ('#') Hashtag renders a line not code.
+# 'ctrl + /' does exact same thing as hashtag
