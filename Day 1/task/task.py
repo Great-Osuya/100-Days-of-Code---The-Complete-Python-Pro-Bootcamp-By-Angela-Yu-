b00994955cb024e69017b77dd8f6bb0231cf9759
Great-Osuya <osuyagoc@gmail.com>
2026-03-19 18:57:45 +0100

    ---learn string manipulation---

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,19 @@
-# input() is a function that gives the user a prompt that they fill or answer
-print("Hello " + input("What is your name?"))
-print("Hello " + input("What is your name?") + "!")
-# ('#') Hashtag renders a line not code
-# 'ctrl + /' does exact same thing as hashtag
+# Write your code below this line 👇
+# \n to make multiple lines with only one line of code. Leave no space between the last variable and "\n"
+# + sign is squeeze multiple strings into one line
+print("Hello" + " " + "Great")
+
+print("Hello Great\nHello Rose\nHello Matthew")
+print("1. Hello World!\n2. Hello World!\n3. Hello World!\n4. Hello World!")
+
+print("Hello Ada!\n" + "0 " + "Hello Ada!\n" + "Hello Ada!")
+
+# the three below have the exact same result
+print("Hello " + "Greg")
+print("Hello" + " Greg")
+print("Hello" + " " + "Greg")
+
+# observer that '\n + space + \n' gives an extra line between the strings. Hence, empty space is a string on its own
+print("Hello Wendy!\n \nHello Wendy!")
+
+print("Hello Angela." + " I am excited to meet you")
