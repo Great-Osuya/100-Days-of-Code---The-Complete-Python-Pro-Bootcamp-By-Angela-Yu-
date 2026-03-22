cf650b45e5a70d3c90210f5a2a859dbb585b6af0
Great-Osuya <osuyagoc@gmail.com>
2026-03-22 18:09:22 +0100

    ---learn Pseudorandom Number Generators---

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,4 +1,30 @@
 import random
-#Mersene Twister is the Pseudorandom Number Generators that python uses
 
-print(random.randint(1,10))
+
+
+#Mersene Twister is the Pseudorandom Number Generators that python uses
+#Read this https://en.wikipedia.org/wiki/Mersenne_Twister
+#Watch this https://www.youtube.com/watch?v=GtOt7EBNEwQ&ab_channel=KhanAcademyLabs
+#For the various functions for random numbers, read this document https://docs.python.org/3/library/random.html ...
+#The random module makes it easy to use Pseudorandom number generators with learning the complex math
+
+print(random.randint(1,10)) #a <= N <= b (where a, b and N are always integers)
+#This generates a random integer between and inclusive of 1 and 10
+
+print(random.random()) # 0.0 <= N < 1.0
+#random.random() function generates a number between 0 and 1, but always less than 1
+print(random.random() * 10 ) #multiplying by 10 increases the range to be "0.0 <= N < 10"
+
+print(random.uniform(0.1,10.5))  #a <= N <= b (where a, b and N are floating )[can also be integers, but it will......
+# generate floats]
+
+import My_Module
+print(My_Module.Goldfish)
+# So basically, this is how it works, you create a file, then you import it. Type in the name of the file, then a dot...
+#followed by the name of a component of the file (a content within the file). e.g.
+#Assuming the name of the file is <filename> and the content is <content/component>
+#Next type; <Import filename>
+#then to select the specific component you want to highlight or make use of.
+# i.e. <filename.content/component>
+
+
