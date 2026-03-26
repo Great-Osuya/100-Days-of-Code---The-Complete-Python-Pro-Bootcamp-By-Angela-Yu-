e9cf3931e50ddde07044e7152ad4a82ac8dd118f
Great-Osuya <osuyagoc@gmail.com>
2026-03-26 14:04:11 +0100

    Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,14 +1,6 @@
-# This is how I solved the project.
-print("Welcome to GREAT's Band Name Generator.")
-print("What is the name of the city you grew up in?")
-city_name= input()
-print("What is the name of your pet")
-pet_name = input()
-print("Your brand name could be " + city_name + " " + pet_name)
-
-print("\n")
-#The below is how ANGELA YU solved it
-print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's the name of your pet?\n")
-print("Your band name could be " + city + " " + pet)
+# Write your code below this line 👇
+# ("") exclamation marks are required to show that something is a string of characters which the function would act on
+print("Hello World")
+print("Hello Me")
+# ('#') Hashtag renders a line not code.
+# 'ctrl + /' does exact same thing as hashtag
